b9f0a5ec1583052d3ca6bf6e3b7c4e5e7432bcc4
Shantanu Bhattarai <shantanu.bhattarai271@gmail.com>
2020-08-04 20:43:03 +0545

    Updates

---
+++ precache-manifest.4297f55755a4094b6705491db85923f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91398b733a6beaa7c18ade9e9947bd02",
+    "url": "/github-repo-viewer/index.html"
+  },
+  {
+    "revision": "dc58e3bef5e781abc0f7",
+    "url": "/github-repo-viewer/static/css/main.35b28de8.chunk.css"
+  },
+  {
+    "revision": "972fc83c5ac384e65e70",
+    "url": "/github-repo-viewer/static/js/2.3352facc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-repo-viewer/static/js/2.3352facc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc58e3bef5e781abc0f7",
+    "url": "/github-repo-viewer/static/js/main.266e1e8a.chunk.js"
+  },
+  {
+    "revision": "58d406638fc44b9ad450",
+    "url": "/github-repo-viewer/static/js/runtime-main.43df09c8.js"
+  }
+]);
